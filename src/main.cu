7cea7a1ab06355798b9e37e6ef1416bf2f06ef4c
Pritika Vipin <65793273+Pritiks23@users.noreply.github.com>
2026-07-29 13:00:57 -0700

    Update main.cu

--- src/main.cu
+++ src/main.cu
@@ -4,79 +4,209 @@
 
 #include "graph.h"
 #include "pagerank.h"
+#include "benchmark.h"
+
 
 
 AllocatorType parseAllocator(
-    std::string name
+    const std::string& name
 )
 {
 
-    if(name=="cudaMalloc")
+
+    if(name == "cudaMalloc")
+    {
+
         return AllocatorType::CUDA_MALLOC;
 
+    }
 
-    if(name=="cudaMallocAsync")
+
+
+    if(name == "cudaMallocAsync")
+    {
+
         return AllocatorType::CUDA_ASYNC;
 
+    }
 
-    if(name=="unified_memory")
+
+
+    if(name == "memory_pool")
+    {
+
+        return AllocatorType::MEMORY_POOL;
+
+    }
+
+
+
+    if(name == "unified_memory")
+    {
+
         return AllocatorType::UNIFIED;
 
+    }
 
-    return AllocatorType::MEMORY_POOL;
+
+
+    throw std::runtime_error(
+        "Unknown allocator: "
+        + name
+    );
 
 }
 
 
 
-int main(int argc,char** argv)
+int main(int argc, char** argv)
 {
 
 
-    std::string allocator="cudaMalloc";
+    std::cout
+    << "====================================\n"
+    << " GPU Graph Memory Optimization Lab\n"
+    << "====================================\n\n";
 
 
-    std::string file=
+
+    Benchmark::printGPUInfo();
+
+
+
+    std::string allocator_name =
+        "cudaMalloc";
+
+
+
+    std::string graph_file =
         "../data/cit-HepTh.txt";
 
 
 
-    for(int i=0;i<argc;i++)
+    for(int i=1;i<argc;i++)
     {
 
-        if(std::string(argv[i])=="--allocator")
-            allocator=argv[i+1];
+
+        std::string arg =
+            argv[i];
+
+
+        if(arg=="--allocator")
+        {
+
+            allocator_name =
+                argv[++i];
+
+        }
+
+
+        else if(arg=="--graph")
+        {
+
+            graph_file =
+                argv[++i];
+
+        }
 
     }
+
+
+
+    std::cout
+    << "\nSelected allocator: "
+    << allocator_name
+    << "\n";
 
 
 
     CSRGraph graph;
 
 
+
     graph.loadFromEdgeList(
-        file
+        graph_file
     );
+
+
+
+    int iterations = 100;
+
+
+
+    size_t memory_before =
+        Benchmark::getFreeMemory();
 
 
 
     float runtime =
         runPageRank(
+
             graph,
-            100,
+
+            iterations,
+
             parseAllocator(
-                allocator
+                allocator_name
             )
+
         );
 
 
 
+    size_t memory_after =
+        Benchmark::getFreeMemory();
+
+
+
     std::cout
-    << "\nAllocator: "
-    << allocator
-    << "\nRuntime: "
+    << "\n========== RESULTS ==========\n";
+
+
+
+    std::cout
+    << "Allocator: "
+    << allocator_name
+    << "\n";
+
+
+
+    std::cout
+    << "Iterations: "
+    << iterations
+    << "\n";
+
+
+
+    std::cout
+    << "Total Runtime: "
     << runtime
     << " ms\n";
 
+
+
+    std::cout
+    << "Average Iteration: "
+    << runtime / iterations
+    << " ms\n";
+
+
+
+    std::cout
+    << "Approx GPU Memory Used: "
+    << (memory_before - memory_after)
+       /
+       1024.0
+       /
+       1024.0
+    << " MB\n";
+
+
+
+    std::cout
+    << "=============================\n";
+
+
+    return 0;
 
 }
