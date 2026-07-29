63077727caf116d7a95bb9c771297bc77ca97b84
Pritika Vipin <65793273+Pritiks23@users.noreply.github.com>
2026-07-29 12:53:21 -0700

    Update pagerank.h

--- src/pagerank.h
+++ src/pagerank.h
@@ -1,82 +1,26 @@
 #pragma once
 
 
-#include <cuda_runtime.h>
+#include "graph.h"
 
 
-class GPUMemoryPool
+enum class AllocatorType
 {
 
+    CUDA_MALLOC,
 
-private:
+    CUDA_ASYNC,
 
-    char* memory;
+    MEMORY_POOL,
 
-    size_t capacity;
-
-    size_t offset;
-
-
-
-public:
-
-
-    GPUMemoryPool(size_t bytes)
-        :
-        capacity(bytes),
-        offset(0)
-    {
-
-        cudaMalloc(
-            &memory,
-            bytes
-        );
-
-    }
-
-
-
-    ~GPUMemoryPool()
-    {
-
-        cudaFree(memory);
-
-    }
-
-
-
-    void* allocate(size_t bytes)
-    {
-
-
-        if(offset + bytes > capacity)
-        {
-
-            throw std::runtime_error(
-                "Pool exhausted"
-            );
-
-        }
-
-
-        void* ptr =
-            memory + offset;
-
-
-        offset += bytes;
-
-
-        return ptr;
-
-    }
-
-
-
-    void reset()
-    {
-
-        offset=0;
-
-    }
+    UNIFIED
 
 };
+
+
+
+float runPageRank(
+    CSRGraph& graph,
+    int iterations,
+    AllocatorType type
+);
