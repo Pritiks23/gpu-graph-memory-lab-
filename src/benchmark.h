0c8d952034a069a7cd71aafb29614df673e34064
Pritika Vipin <65793273+Pritiks23@users.noreply.github.com>
2026-07-29 12:46:01 -0700

    Update benchmark.h

--- src/benchmark.h
+++ src/benchmark.h
@@ -3,120 +3,61 @@
 #include <cuda_runtime.h>
 
 #include <iostream>
-#include <chrono>
-
-
-struct BenchmarkResult
-{
-
-    double total_ms;
-
-    double avg_iteration_ms;
-
-
-    size_t start_free_memory;
-
-    size_t end_free_memory;
-
-    size_t peak_memory_used;
-
-
-
-    int allocation_count;
-
-    size_t allocated_bytes;
-
-};
 
 
 
 class Benchmark
 {
 
-private:
-
-    cudaEvent_t start;
-
-    cudaEvent_t stop;
-
-
-
 public:
-
-
-    Benchmark()
-    {
-
-        cudaEventCreate(&start);
-
-        cudaEventCreate(&stop);
-
-    }
-
-
-
-    ~Benchmark()
-    {
-
-        cudaEventDestroy(start);
-
-        cudaEventDestroy(stop);
-
-    }
-
-
-
-    void begin()
-    {
-
-        cudaEventRecord(start);
-
-    }
-
-
-
-    float end()
-    {
-
-        cudaEventRecord(stop);
-
-        cudaEventSynchronize(stop);
-
-
-        float ms;
-
-
-        cudaEventElapsedTime(
-            &ms,
-            start,
-            stop
-        );
-
-
-        return ms;
-
-    }
-
 
 
     static size_t getFreeMemory()
     {
 
-        size_t free_mem;
+        size_t free_memory;
 
-        size_t total_mem;
+        size_t total_memory;
+
 
 
         cudaMemGetInfo(
-            &free_mem,
-            &total_mem
+            &free_memory,
+            &total_memory
         );
 
 
-        return free_mem;
+        return free_memory;
 
     }
 
 
+
+    static void printGPUInfo()
+    {
+
+        cudaDeviceProp prop;
+
+
+        cudaGetDeviceProperties(
+            &prop,
+            0
+        );
+
+
+        std::cout
+            << "GPU: "
+            << prop.name
+            << "\n";
+
+
+        std::cout
+            << "Memory: "
+            << prop.totalGlobalMem
+              /
+              (1024*1024)
+            << " MB\n";
+
+    }
 
 };
